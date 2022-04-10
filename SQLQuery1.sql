dafcbdf324d34cd74c978fccc56eb54fda769219
FarzinRad <Farzinvado@yahoo.com>
2022-04-11 02:16:41 +0430

    compare total death and death precentatage

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -10,3 +10,10 @@
 SELECT location, date, total_cases, new_cases, total_deaths, population
 FROM Covid19..CovidDeath
 ORDER BY 1,2
+
+-- Compare total cases VS total death and find the precentage:
+
+SELECT location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 AS death_precentage
+FROM Covid19..CovidDeath
+WHERE location LIKE 'Turkey'
+ORDER BY 1,2
